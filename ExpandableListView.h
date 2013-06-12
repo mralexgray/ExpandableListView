15fa587f4f75fac7524bdc594f1e780628c7781e
Alex Gray <alex@mrgray.com>
2013-06-12 13:48:08 -0400

    fuckup brnach

--- ExpandableListView.h
+++ ExpandableListView.h
@@ -7,34 +7,59 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <AtoZ/AtoZ.h>
 
 @class ExpandableListView;
-
-/// @brief Data source for expandable list view. Data source should implement three mandatory methods returning number of views, view for given index, title of view at given index.
+// @brief Data source for expandable list view. Data source should implement three mandatory methods returning number of views, view for given index, title of view at given index.
 @protocol ExpandableListDataSource <NSObject>
-
-/// Method returning number of views to show in expandable list.
-/// @param listView list view requesting information
-/// @return number of views
-- (NSInteger) numberOfViewsInExpandableList:(ExpandableListView *)listView;
-
-/// Method returning title of view to show in expandable list an given index.
-/// @param listView list view requesting information
-/// @param index index of requested view
-- (NSString *)expandableList:(ExpandableListView *)listView titleAtIndex:(NSInteger)index;
-
-/// Method returning view to show in expandable list an given index.
-/// @param listView list view requesting information
-/// @param index index of requested view
-- (NSView *)expandableList:(ExpandableListView *)listView viewAtIndex:(NSInteger)index;
+/* Method returning number of views to show in expandable list.
+ @param listView list view requesting information
+ @return number of views */
+-  (NSI) numberOfViewsInExpandableList:(ExpandableListView*)lv;
+/* Method returning title of view to show in expandable list an given index.
+ @param listView list view requesting information
+ @param index index of requested view */
+- (NSS*) expandableList:(ExpandableListView*)lv titleAtIndex:(NSI)idx;
+/* Method returning view to show in expandable list an given index.
+ @param listView list view requesting information
+ @param index index of requested view */
+- (NSV*) expandableList:(ExpandableListView*)lv viewAtIndex:(NSI)idx;
+- (BOOL) expandableList:(ExpandableListView*)lv shouldExpandViewAtIndex:(NSI)idx;
 @end
 
-/// @brief ExpandableListView is a visual component displaying list of views which can be expanded/collapsed by user clicking on view's header. Example of such behavior is the Inspector in XCode.
+	// @brief ExpandableListView is a visual component displaying list of views which can be expanded/collapsed by user clicking on view's header. Example of such behavior is the Inspector in XCode.
 @interface ExpandableListView : NSView
+	//+ (instancetype) expandableViewInView:(NSV*)v withNode:(AZNode*)node;
+@property (weak) AZNode *node;
 
-/// Data source for expandable list. This property is declared as IBOutlet so it can be set in Interface Builder.
-@property (nonatomic, assign) IBOutlet id <ExpandableListDataSource> dataSource;
-
-/// Method to refresh list content if some data in data source was changed.
+	// Data source for expandable list. This property is declared as IBOutlet so it can be set in Interface Builder.
+@property (nonatomic, unsafe_unretained) IBOutlet id <ExpandableListDataSource> dataSource;
+	// Method to refresh list content if some data in data source was changed.
 - (void)reloadData;
 @end
+//
+///// Method returning number of views to show in expandable list.
+///// @param listView list view requesting information
+///// @return number of views
+//- (NSInteger) numberOfViewsInExpandableList:(ExpandableListView *)listView;
+//
+///// Method returning title of view to show in expandable list an given index.
+///// @param listView list view requesting information
+///// @param index index of requested view
+//- (NSString *)expandableList:(ExpandableListView *)listView titleAtIndex:(NSInteger)index;
+//
+///// Method returning view to show in expandable list an given index.
+///// @param listView list view requesting information
+///// @param index index of requested view
+//- (NSView *)expandableList:(ExpandableListView *)listView viewAtIndex:(NSInteger)index;
+//@end
+//
+///// @brief ExpandableListView is a visual component displaying list of views which can be expanded/collapsed by user clicking on view's header. Example of such behavior is the Inspector in XCode.
+//@interface ExpandableListView : NSView
+//
+///// Data source for expandable list. This property is declared as IBOutlet so it can be set in Interface Builder.
+//@property (nonatomic, assign) IBOutlet id <ExpandableListDataSource> dataSource;
+//
+///// Method to refresh list content if some data in data source was changed.
+//- (void)reloadData;
+//@end
--- TestApp/AppDelegate.h
+++ TestApp/AppDelegate.h
@@ -11,13 +11,35 @@
 
 //@class ExpandableTableView;
 
+//@interface AppDelegate : NSObject <NSApplicationDelegate, ExpandableListDataSource>
+//
+//@property (assign) IBOutlet NSWindow *window;
+//
+//@property (nonatomic, retain) IBOutlet NSView *cell1;
+//@property (nonatomic, retain) IBOutlet NSView *cell2;
+//@property (nonatomic, retain) IBOutlet NSView *cell3;
+//@property (nonatomic, retain) IBOutlet ExpandableListView *listView;
+//
+//@end
+
+@interface AZNodeView : NSObject <ExpandableListDataSource>
++ (instancetype) nodeViewInView:(NSV*)host withFrame:(NSRect)frame usingNode:(AZNode*)node;
+@property (strong, nonatomic) AZNode *node;
+@property (strong, nonatomic) ExpandableListView *view;
+@end
+
+	//@class ExpandableTableView;
+
 @interface AppDelegate : NSObject <NSApplicationDelegate, ExpandableListDataSource>
 
-@property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSWindow *window;
 
-@property (nonatomic, retain) IBOutlet NSView *cell1;
-@property (nonatomic, retain) IBOutlet NSView *cell2;
-@property (nonatomic, retain) IBOutlet NSView *cell3;
-@property (nonatomic, retain) IBOutlet ExpandableListView *listView;
+@property (nonatomic, strong) IBOutlet NSView *cell1;
+@property (nonatomic, strong) IBOutlet NSView *cell2;
+@property (nonatomic, strong) IBOutlet NSView *cell3;
+@property (nonatomic, strong) IBOutlet ExpandableListView *listView;
+@property (nonatomic, strong) IBOutlet ExpandableListView *listView2;
 
+@property (strong) DefinitionController *d;
+@property (strong) AZNodeView *nv;
 @end
