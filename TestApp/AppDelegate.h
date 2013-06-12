dd4589f40e4ec9509fa12a9dee9423896b0f85e2
Alex Gray <alex@mrgray.com>
2013-06-12 13:20:30 -0400

    Revert "commit to go ing back"
    This reverts commit f5bb7ba113ef6407d9d827ce3a5d6c5c9839fb6d.

--- TestApp/AppDelegate.h
+++ TestApp/AppDelegate.h
@@ -7,30 +7,17 @@
 //
 
 #import <Cocoa/Cocoa.h>
-//#import <SNRHUDKit/SNRHUDKit.h>
-#import <AtoZ/AtoZ.h>
-
 #import "ExpandableListView.h"
-
-
-@interface AZNodeView : NSObject <ExpandableListDataSource>
-+ (instancetype) nodeViewInView:(NSV*)host withFrame:(NSRect)frame usingNode:(AZNode*)node;
-@property (strong, nonatomic) AZNode *node;
-@property (strong, nonatomic) ExpandableListView *view;
-@end
 
 //@class ExpandableTableView;
 
 @interface AppDelegate : NSObject <NSApplicationDelegate, ExpandableListDataSource>
 
-@property (weak) IBOutlet NSWindow *window;
+@property (assign) IBOutlet NSWindow *window;
 
-@property (nonatomic, strong) IBOutlet NSView *cell1;
-@property (nonatomic, strong) IBOutlet NSView *cell2;
-@property (nonatomic, strong) IBOutlet NSView *cell3;
-@property (nonatomic, strong) IBOutlet ExpandableListView *listView;
-@property (nonatomic, strong) IBOutlet ExpandableListView *listView2;
+@property (nonatomic, retain) IBOutlet NSView *cell1;
+@property (nonatomic, retain) IBOutlet NSView *cell2;
+@property (nonatomic, retain) IBOutlet NSView *cell3;
+@property (nonatomic, retain) IBOutlet ExpandableListView *listView;
 
-@property (strong) DefinitionController *d;
-@property (strong) AZNodeView *nv;
 @end
